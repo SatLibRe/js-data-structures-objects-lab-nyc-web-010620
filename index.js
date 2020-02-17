8f77d8db5d1ad19799d9e0e9519fd765ed6d8e2e
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 00:24:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  const newObj = Object.assign({}, driver, { [key]: "" } )
+  const newObj = Object.assign({}, driver)
   delete newObj.key
   return newObj
 }
